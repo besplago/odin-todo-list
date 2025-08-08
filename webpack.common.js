004406d8855917107dd46826d20e08e2473f8c88
besplago <bruce_esplago@hotmail.com>
2025-08-08 21:57:22 +0200

    feat: html loader for images directly from html

--- webpack.common.js
+++ webpack.common.js
@@ -30,6 +30,18 @@ module.exports = {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
       },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: "file-loader",
+          },
+        ],
+      },
     ],
   },
 };
