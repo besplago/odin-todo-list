e6290a1101a8a95a7701585fcc1d481432d5e72f
besplago <bruce_esplago@hotmail.com>
2025-08-15 21:33:46 +0200

    chore: removed comments

--- src/js/views/EditTaskView.js
+++ src/js/views/EditTaskView.js
@@ -1,7 +1,3 @@
-// 1. New task selected updated in task model
-// 2. Controller relays this news to this view
-// 3. This view opens (unhides) the edit pane
-
 export class EditTaskView {
   constructor(editPaneSelector) {
     this.paneAside = document.querySelector(editPaneSelector);
